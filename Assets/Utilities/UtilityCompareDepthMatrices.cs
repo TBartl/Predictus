c0b57081803845eb8e1ee2ea7fbb71c7402cad98
Chuxuan Xiong <chuxuanxiong@0587368559.wireless.umich.net>
2017-02-14 13:12:03 -0500

    CompareDepthMatrices finishes

--- Assets/Utilities/UtilityCompareDepthMatrices.cs
+++ Assets/Utilities/UtilityCompareDepthMatrices.cs
@@ -11,7 +11,22 @@ public class UtilityCompareDepthMatrices : MonoBehaviour {
     }
 
     public DepthMatrixData Compare(DepthMatrixData from, DepthMatrixData to) {
-        //to.depthsp[0, 0] == -1;
-        return null;
+        //to.depths[0, 0] == -1 means no blocks
+
+		//two rectangles, do substraction to find out the diff
+		int width = from.depths.GetLength(0);
+		int height = from.depths.GetLength(1);
+		DepthMatrixData diff = new DepthMatrixData ();
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				//if any one of the depth value is -1, assign 0 to diff
+				if (from.depths [x, y] == -1 || to.depths [x, y] == -1) {
+					diff.depths [x, y] = 0;
+					continue;
+				}
+				diff.depths[x, y] = from.depths [x, y] - to.depths [x, y];
+			}
+		}
+        return diff;
     }
 }
