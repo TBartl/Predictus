22fcec1f0b7b106e48008e0bebbbf5327042064f
tanselyn <hw.wwang@gmail.com>
2017-02-15 18:45:30 -0500

    ApplyDepthMatrix

--- Assets/Utilities/UtilityApplyDepthMatrixToMesh.cs
+++ Assets/Utilities/UtilityApplyDepthMatrixToMesh.cs
@@ -10,10 +10,39 @@ public class UtilityApplyDepthMatrixToMesh : MonoBehaviour {
         S = this;
     }
 
-    public Mesh Apply(Mesh originalMesh, DepthMatrixData depthData) {
+	public IntVector3 TransformToIntVector(Vector3 point) { 
+		Vector3 testPoint = point / VoxData.scale;        
+		return new IntVector3(Mathf.RoundToInt(testPoint.x), Mathf.RoundToInt(testPoint.y), Mathf.RoundToInt(testPoint.z));
+	}
 
-        return null;
+
+    public Mesh Apply(Mesh originalMesh, DepthMatrixData depthData) {
+		Vector3[] vertices = originalMesh.vertices;
+		Vector3 offset = (-new Vector3(depthData.GetWidth(), depthData.GetHeight(), 0) + Vector3.one) * VoxData.scale / 2f;
+
+		for (int i = 0; i < vertices.Length; i++) {
+			IntVector3 v = TransformToIntVector (vertices [i] - offset);
+			if (v.x < 0 || v.x >= depthData.GetWidth () ||
+			    v.y < 0 || v.y >= depthData.GetHeight ()) {
+				continue;
+			} else {
+				vertices [i] += (Vector3.back * VoxData.scale * depthData.depths[v.x, v.y]);
+			}
+
+		}
+		Mesh updatedMesh = new Mesh(); 
+		transform.gameObject.AddComponent<MeshFilter>();
+		updatedMesh = GetComponent<MeshFilter> ().mesh;
+		int[] indices = originalMesh.GetIndices (0);
+		updatedMesh.vertices = vertices;
+		updatedMesh.triangles = indices;
+
+		updatedMesh.RecalculateBounds ();
+		updatedMesh.RecalculateNormals ();
+		return updatedMesh;
+	
     }
+		
 
 
 }
