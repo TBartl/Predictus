610914decf893c5760c70f8853933eebd1e998e9
tanselyn <hw.wwang@gmail.com>
2017-02-16 12:59:29 -0500

    Fix mesh back

--- Assets/Utilities/UtilityApplyDepthMatrixToMesh.cs
+++ Assets/Utilities/UtilityApplyDepthMatrixToMesh.cs
@@ -18,17 +18,23 @@ public class UtilityApplyDepthMatrixToMesh : MonoBehaviour {
 
     public Mesh Apply(Mesh originalMesh, DepthMatrixData depthData) {
 		Vector3[] vertices = originalMesh.vertices;
+        Vector3[] normals = originalMesh.normals;
 		Vector3 offset = (-new Vector3(depthData.GetWidth(), depthData.GetHeight(), 0) + Vector3.one) * VoxData.scale / 2f;
 
 		for (int i = 0; i < vertices.Length; i++) {
 			IntVector3 v = TransformToIntVector (vertices [i] - offset);
-			if (v.x < 0 || v.x >= depthData.GetWidth () ||
-			    v.y < 0 || v.y >= depthData.GetHeight ()) {
-				continue;
-			} else {
-				vertices [i] += (Vector3.back * VoxData.scale * depthData.depths[v.x, v.y]);
-			}
-
+            if (Vector3.Dot(normals[i], Vector3.forward) < 0)
+            {
+                if (v.x < 0 || v.x >= depthData.GetWidth() ||
+                    v.y < 0 || v.y >= depthData.GetHeight())
+                {
+                    continue;
+                }
+                else
+                {
+                    vertices[i] += (Vector3.back * VoxData.scale * depthData.depths[v.x, v.y]);
+                }
+            }
 		}
 		Mesh updatedMesh = new Mesh(); 
 		transform.gameObject.AddComponent<MeshFilter>();
