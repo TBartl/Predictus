69c5464f6c72dc63675eaa32577e67684e347531
TBartl <tbartl@umich.edu>
2017-03-07 10:35:40 -0500

    OpenOBJ

--- Assets/Utilities/UtilityOpenOBJ.cs
+++ Assets/Utilities/UtilityOpenOBJ.cs
@@ -6,8 +6,15 @@ public class UtilityOpenOBJ : MonoBehaviour {
     
     public static Mesh OpenOBJ() {
         if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+            // Here you'll want to open up the File Dialogue to navigate to a .obj file
+            // I've got to be honest with you on this one, I have absolutely no idea how you would do this
+            // Instead of googling for a Unity specific tutorial, I would just look at how you can do this in C#
+            // Hopefully the System function you'll end up using will lock Unity from doing anything further
+            //  but if Unity continues to run let me know and I'll see if I can make this into a coroutine or something where it's used.
+
             return null;
-        } else {
+        }
+        else {
             return null;
         }
     }
