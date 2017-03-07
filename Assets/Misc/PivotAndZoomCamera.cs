14c093c602d10d88ba76bfec6a2337df357ec565
TBartl <tbartl@umich.edu>
2017-03-07 11:15:51 -0500

    Pivot stuff

---
+++ Assets/Misc/PivotAndZoomCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotAndZoomCamera : MonoBehaviour {
+    Vector3 rotation = Vector3.zero;
+    public float rotationSpeed;
+    public float zoomSpeed;
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1)) {
+            rotation += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * rotationSpeed;
+            this.transform.rotation = Quaternion.Euler(rotation);
+        }
+        this.transform.localScale -= Vector3.one * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+    }
+}
--- Assets/Utilities/UtilityOpenOBJ.cs
+++ Assets/Utilities/UtilityOpenOBJ.cs
@@ -12,6 +12,14 @@ public class UtilityOpenOBJ : MonoBehaviour {
             // Hopefully the System function you'll end up using will lock Unity from doing anything further
             //  but if Unity continues to run let me know and I'll see if I can make this into a coroutine or something where it's used.
 
+            // Once you've actually opened the file it should be readable just like any other text file
+            // There are a ton of tutorials on line that explain the 3D OBJ format so look to those first.
+            // Unfortunately there are some different interpertations of the format and what should be included,
+            //  so you may want to look at the OBJ's in the box folder to see how their program outputs it
+
+            // Finally we'll (Chuxuan) need to make a Mesh out of this
+            // In addition to writing the vertices and indices to a new mesh, you'll also want to make sure you recalculate the mesh bounds and normals
+
             return null;
         }
         else {
