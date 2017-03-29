e5911a4daa3196ba889a010c610a18d36de1924b
TBartl <tbartl@umich.edu>
2017-03-28 20:09:20 -0400

    Correct screen stuff

---
+++ Assets/Utilities/UtilityCopyMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+using System.IO;
+using SimpleFileBrowser;
+using System;
+
+public class UtilityCopyMesh : MonoBehaviour {
+
+    // I want to see if this will fix an error, but hopefully we won't have to use this script in the long run
+    public static Mesh Copy(Mesh original) {
+        Mesh clone = new Mesh();
+        clone.SetVertices(new List<Vector3>(original.vertices));
+        clone.SetTriangles(new List<int>(original.triangles), 0);
+        clone.RecalculateBounds();
+        clone.RecalculateNormals();
+        clone.name = original.name + " (clone)";
+        return clone;
+    }
+}
