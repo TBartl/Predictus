ff6d3b6b2c01472e70396d6e63b78146a24de5c2
TBartl <tbartl@umich.edu>
2017-03-29 17:15:17 -0400

    Fix mesh scene done

--- Assets/UI/Scripts/ButtonProcessAndUpdateMesh.cs
+++ Assets/UI/Scripts/ButtonProcessAndUpdateMesh.cs
@@ -12,8 +12,6 @@ public class ButtonProcessAndUpdateMesh : MonoBehaviour {
     public void OnClick() {
         DepthMatrixData toApply = null;
 		SoundManager.SM.PlayTransformSound ();
-        
-        ApplyMeshTranslationAndRotation();
 
         //if (debugAfterMesh) {
         //    DepthMatrixData fromDepths = UtilityVoxelizeAndGetDepthMatrix.S.Process(fromScreen.mesh);
@@ -28,11 +26,5 @@ public class ButtonProcessAndUpdateMesh : MonoBehaviour {
         toPredictedAfter.mesh = UtilityApplyDepthMatrixToMesh.Apply(fromScreen.mesh, toApply);
     }
 
-    void ApplyMeshTranslationAndRotation() {
-        List<Vector3> vertices = new List<Vector3>(fromScreen.mesh.vertices);
-        for (int i = 0; i < vertices.Count; i++) { 
-            vertices[i] = fromScreen.transform.TransformPoint(vertices[i]);
-        }
-        fromScreen.mesh.SetVertices(vertices);
-    }
+    
 }
--- Assets/UI/Scripts/OrientInputMesh.cs
+++ Assets/UI/Scripts/OrientInputMesh.cs
@@ -5,71 +5,58 @@ using UnityEngine;
 public class OrientInputMesh : MonoBehaviour {
 
     Vector3 initialMousePos;
-    Vector3 initialObjectPos;
     Vector3 offset;
-    bool modifyPositionNotRotation = true;
+    public bool modifyPositionNotRotation = true;
+    MeshFilter meshFilter;
+    public float rotationSpeed = 20f;
 
-    void Update() {
-        // Eventually we'll want to have this as a UI button, but for now just press space to change between position and rotation changes
-        if (Input.GetKeyDown(KeyCode.Space))
-            modifyPositionNotRotation = !modifyPositionNotRotation;
-
-        if (modifyPositionNotRotation)
-            UpdatePosition();
-        else
-            UpdateRotation();
+    void Awake() {
+        meshFilter = this.GetComponent<MeshFilter>();
     }
+    
 
-    private void OnMouseDrag()
-    {
-        Vector3 mousePos = initialMousePos;
-        Plane objectPlane = new Plane(Vector3.back, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float rayDistance;
-        if (objectPlane.Raycast(ray, out rayDistance))
-        {
-            mousePos = ray.GetPoint(rayDistance);
-        }
-
-
+    private void OnMouseDrag() {
+        Vector3 mousePos = GetMousePos();
         offset = mousePos - initialMousePos;
         offset.z = 0.0f;
-        transform.position = initialObjectPos + offset;
+        if (modifyPositionNotRotation) {
+            transform.position = offset;
+        }
+        else {
+            transform.rotation = Quaternion.Euler(0, -offset.x * rotationSpeed, 0);
+
+        }
 
     }
 
-    private void OnMouseDown()
-    {
+    private void OnMouseDown() {
+        Vector3 mousePos = GetMousePos();
+        initialMousePos = mousePos;
+    }
+
+    Vector3 GetMousePos() {
         Vector3 mousePos = initialMousePos;
         Plane objectPlane = new Plane(Vector3.back, transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float rayDistance;
-        if (objectPlane.Raycast(ray, out rayDistance))
-        {
+        if (objectPlane.Raycast(ray, out rayDistance)) {
             mousePos = ray.GetPoint(rayDistance);
         }
-        initialMousePos = mousePos;
-        initialObjectPos = transform.position;
+        return mousePos;
     }
 
-    // Update is called once per frame
-
-void UpdatePosition() {
-        // Here you'll want to implement some way for the user to translate this object around.
-        // There's a lot of ways to do this and it's pretty up to you, but I would look at something like the Unity Editor's system
-        //   where you just have grabbable arrows you can grab to drag the object on a given axis.
-
-        // Input.GetMouseButtonDown(0) returns true the frame the left mouse button is pressed
-        // Input.GetMouseButton(0) returns true every frame the left mouse button is held down
-        // This object has a capsule collider and is on layer "Rotatable"
-        //   you may want to look into raycasting to see if a mouse is hovering over this object.
-        // Changing the position is as simple as doing something like this.transform.position += Vector3.up * Time.deltaTime;
-
-        transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel"));
-
+    void OnMouseUp() {
+        ApplyMeshTranslationAndRotation();
+        this.transform.position = Vector3.zero;
+        this.transform.rotation = Quaternion.identity;
     }
 
-    void UpdateRotation() {
-
+    void ApplyMeshTranslationAndRotation() {
+        List<Vector3> vertices = new List<Vector3>(meshFilter.mesh.vertices);
+        for (int i = 0; i < vertices.Count; i++) {
+            vertices[i] = transform.TransformPoint(vertices[i]);
+        }
+        meshFilter.mesh.SetVertices(vertices);
+        meshFilter.mesh.RecalculateNormals();
     }
 }
