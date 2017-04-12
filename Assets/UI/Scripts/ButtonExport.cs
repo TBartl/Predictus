a3671ed1d7185d772e2f1e0e67d26e7e7c32d5d6
Brian <yebri@umichedu>
2017-04-12 17:59:41 -0400

    More exports

--- Assets/UI/Scripts/ButtonExport.cs
+++ Assets/UI/Scripts/ButtonExport.cs
@@ -7,7 +7,16 @@ public class ButtonExport : MonoBehaviour {
 
 	public void OnClick() {
 
+		StartCoroutine (SaveFileBrowser ());
 
+	}
 
+	IEnumerator SaveFileBrowser() {
+		
+		ScreenManager.S.transitioning = true;
+
+		yield return FileBrowser.WaitForSaveDialog (false, null, "Save File", "Save");
+
+		ScreenManager.S.transitioning = false;
 	}
 }
