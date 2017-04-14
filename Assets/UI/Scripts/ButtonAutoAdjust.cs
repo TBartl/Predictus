c4a7fe6c5b38651c9973921746cdb104eef02d7f
TBartl <tbartl@umich.edu>
2017-04-14 13:45:51 -0400

    updated gitignore

--- Assets/UI/Scripts/ButtonAutoAdjust.cs
+++ Assets/UI/Scripts/ButtonAutoAdjust.cs
@@ -14,10 +14,12 @@ public class ButtonAutoAdjust : MonoBehaviour {
     public void OnClick() {
         if (ScreenManager.S.IsTransitioning())
             return;
-        mf.mesh.RecalculateBounds();
+        SoundManager.SM.PlayButtonSound();
 
+        mf.mesh.RecalculateBounds();
         Vector3 boundsSize = mf.mesh.bounds.size;
 
+        // Rotate to be on the right axis
         if      (boundsSize.x < boundsSize.y && boundsSize.y < boundsSize.z)//x<y<z
             orientInputMesh.transform.Rotate(90, 0, 90);
         else if (boundsSize.x < boundsSize.z && boundsSize.z < boundsSize.y)//x<z<y
@@ -30,10 +32,13 @@ public class ButtonAutoAdjust : MonoBehaviour {
             orientInputMesh.transform.Rotate(0, 0, 0); // Correct orientation already
         else if (boundsSize.z < boundsSize.y && boundsSize.y < boundsSize.x)//z<y<x
             orientInputMesh.transform.Rotate(0, 0, 90);
-
         orientInputMesh.ApplyMeshTranslationAndRotation();
 
-        SoundManager.SM.PlayButtonSound();
-        //orientInputMesh.modifyPositionNotRotation = !orientInputMesh.modifyPositionNotRotation;
+        float centerOfY = 0;
+        List<Vector3> verts = new List<Vector3>(mf.mesh.vertices);
+        foreach (Vector3 vert in verts) {
+            centerOfY += vert.y * Mathf.Abs(vert.x);
+        }
+        
     }
 }
