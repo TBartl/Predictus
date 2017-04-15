41cec8a54382c550b8f8fe9ada6575fd94450b41
Brian <yebri@umichedu>
2017-04-14 21:17:23 -0400

    zooming for touch devices

--- Assets/Misc/PivotAndZoomCamera.cs
+++ Assets/Misc/PivotAndZoomCamera.cs
@@ -15,7 +15,15 @@ public class PivotAndZoomCamera : MonoBehaviour {
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.identity, .5f * Time.deltaTime);
         }
 		if (!ScreenManager.S.IsTransitioning ()) {
-			this.transform.localScale -= Vector3.one * Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+
+			#if UNITY_IOS
+			HandleTouchZooming();
+			#elif UNITY_ANDROID
+			HandleTouchZooming();
+			#else
+			HandleMouseZooming();
+
+			#endif
 
 			if (this.transform.localScale.x > 4.8f)
 				this.transform.localScale = Vector3.one * 4.75f;
@@ -23,4 +31,37 @@ public class PivotAndZoomCamera : MonoBehaviour {
 				this.transform.localScale = Vector3.one * 0.75f;
 		}
     }
+
+	void HandleMouseZooming() {
+		this.transform.localScale -= Vector3.one * Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+	}
+
+	void HandleTouchZooming() {
+		int fingercount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				fingercount++;
+			}
+		}
+
+		if (fingercount == 2) {
+
+			// Store both touches.
+			Touch touchZero = Input.GetTouch(0);
+			Touch touchOne = Input.GetTouch(1);
+
+			// Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			// Find the magnitude of the vector (the distance) between the touches in each frame.
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			// Find the difference in the distances between each frame.
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+			this.transform.localScale -= Vector3.one * deltaMagnitudeDiff * zoomSpeed;
+		}
+	}
 }
