97dbad4651a79ce6a613e9268b6832d44baf962d
Brian <yebri@umichedu>
2017-04-14 21:42:58 -0400

    zoom again

--- Assets/Misc/PivotAndZoomCamera.cs
+++ Assets/Misc/PivotAndZoomCamera.cs
@@ -62,7 +62,7 @@ public class PivotAndZoomCamera : MonoBehaviour {
 			// Find the difference in the distances between each frame.
 			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-			this.transform.localScale -= Vector3.one * deltaMagnitudeDiff * (zoomSpeed/10);
+			this.transform.localScale -= Vector3.one * deltaMagnitudeDiff * (zoomSpeed/10.0f);
 		}
 	}
 }
