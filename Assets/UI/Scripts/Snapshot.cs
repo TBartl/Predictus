8e469e883d1704d063805cc8461bf68f1a53efc5
Brian <yebri@umichedu>
2017-04-15 19:07:04 -0400

    snapshot

--- Assets/UI/Scripts/Snapshot.cs
+++ Assets/UI/Scripts/Snapshot.cs
@@ -1,36 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using SimpleFileBrowser;
 
 public class Snapshot : MonoBehaviour {
 
 	private bool takeHiResShot = false;
+	private bool takeSaveShot = false;
 
     Camera cam;
+	string savePath;
 
     void Awake() {
         cam = this.GetComponent<Camera>();
     }
 
 	public string ScreenShotName(int width, int height) {
+
 		return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
-			Application.dataPath, 
+			savePath, 
 			width, height, 
 			System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+
+//		return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
+//			Application.dataPath, 
+//			width, height, 
+//			System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
 	}
 
 	public void TakeSnapshot() {
 		SoundManager.SM.PlayButtonSound ();
+		StartCoroutine(WaitTakeSnapshot());
+	}
+
+	IEnumerator WaitTakeSnapshot() {
+
+		ScreenManager.S.transitioning = true;
+
+		yield return FileBrowser.WaitForSaveDialog (false, null, "Save File", "Save");
+
+		if (FileBrowser.Success == false) {
+			ScreenManager.S.transitioning = false;
+			yield break;
+		}
+
+		savePath = FileBrowser.Result;
+		Debug.Log (savePath);
+		ScreenManager.S.transitioning = false;
 		takeHiResShot = true;
 	}
 
-//	void Update() {
-//		//Debug.Log(Input.GetKeyDown("k"));
-//		if (Input.GetKeyDown (KeyCode.K)) {
-//			TakeHiResShot ();
-//			Debug.Log ("key pressed");
-//		}
-//	}
+	public void TakeSaveSnapshot(string savePathIn) {
+		takeSaveShot = true;
+		savePath = savePathIn;
+	}
 
 	void LateUpdate() {
 		
@@ -49,5 +72,19 @@ public class Snapshot : MonoBehaviour {
 			takeHiResShot = false;
 		}
 
+		// saving snapshot
+		if (takeSaveShot) {
+			RenderTexture rt = cam.targetTexture;
+			RenderTexture.active = rt;
+			Texture2D screenShot = new Texture2D(rt.width, rt.height);
+			screenShot.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+			screenShot.Apply ();
+
+
+			byte[] bytes = screenShot.EncodeToPNG();
+			string filename = savePath + "previewImage.png";
+			System.IO.File.WriteAllBytes(filename, bytes);
+			takeSaveShot = false;
+		}
 	}
 }
